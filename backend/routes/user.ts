5acb69315bd07fa9f425ec23f6f18d258932acb6
cyprientvl <cyprien.tavel@gmail.com>
2024-03-22 15:18:31 +0100

    error

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { User, Product, Bid } from '../orm/index.js'
+import { MissingUser } from '../error/error.js'
 
 const router = express.Router()
 
@@ -8,16 +9,16 @@ router.get('/api/users/:userId', async (req, res) => {
   try{
 
     const user = await User.findByPk(req.params.userId);
-    
-    //if(!user) throw new MissingUser()
+    console.log(user);
+    if(!user) throw new MissingUser()
     return res.status(200).json(user);
 
 
   }catch(e){
 
-    /*if(e instanceof MissingUser){
+    if(e instanceof MissingUser){
       return res.status(404).json({error: "User not found"})
-    }*/
+    }
     return res.status(500).json({error: "Internal server error"})
   }
   
