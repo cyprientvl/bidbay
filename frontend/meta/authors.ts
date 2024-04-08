ff81bde1f7853cfa355d9a0adc8544c959e6f3ae
p2110156 <quentin.bastos@etu.univ-lyon1.fr>
2024-04-08 14:28:29 +0200

    add file generate 18/18

--- frontend/meta/authors.ts
+++ frontend/meta/authors.ts
@@ -1,5 +1,5 @@
 // Noms et numéros d'étudiant
-export const authors = ["Bastos Quentin (12110156)", "Tavel Cyprien (12202105)"];
+export const authors = ["Bastos Quentin (p2110156)", "Tavel Cyprien (p2202105)"];
 
 // Groupe
 export const group = 2;
